510114abe3106b13d7f545684fe393978cb7b3c8
Divyansh <divyansh@Divyanshs-MacBook-Pro.local>
2026-01-07 16:46:44 +0530

    fix: return jwt token from generatetoken

--- Backend/utils/generatetoken.js
+++ Backend/utils/generatetoken.js
@@ -1,10 +1,12 @@
 import jwt from 'jsonwebtoken'
 
-const genratetoken=(id)=>{
-    const token= jwt.sign({id},process.env.JWT_SEC,{
-        expiresIn:"15d"
-    })
+const genratetoken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SEC, {
+    expiresIn: "15d",
+  });
+};
 
-}
+
+
 
 export default genratetoken
