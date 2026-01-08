2df9bdae5a9944055bff43dde55133571f796f84
Divyansh <divyansh@Divyanshs-MacBook-Pro.local>
2026-01-08 11:57:33 +0530

    fix: load env vars correctly and standardize names

--- Backend/index.js
+++ Backend/index.js
@@ -54,7 +54,7 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start Server + Connect DB
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 
 app.listen(PORT, async () => {
   console.log(`✅ Server running on port ${PORT}`);
